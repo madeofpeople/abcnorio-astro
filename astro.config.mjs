d798e712306a6e87d41d4012a0df73894a72a6b6
Thiago de Mello Bueno <thiago@madeofpeople.org>
2026-02-12 17:42:03 +0000

    improves handling of environmental variables during build for deployment and testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,20 @@ import icon from 'astro-icon';
 import { loadEnv } from "vite";
 
 const env = loadEnv(process.env, process.cwd(), "");
-let BUILD_PATH = env.DEV_BUILD_PATH;
 
-switch (env.BUILD_MODE) {
-  case "production":
-    BUILD_PATH = env.PRODUCTION_BUILD_PATH;
-  break;
-  case "staging":
-    BUILD_PATH = env.STAGING_BUILD_PATH;
-  break;
+let BUILD_PATH = env.BUILD_PATH;
+if (!env.BUILD_PATH) {
+  switch (env.BUILD_MODE) {
+    case "production":
+      BUILD_PATH = env.PRODUCTION_BUILD_PATH;
+    break;s
+    case "staging":
+      BUILD_PATH = env.STAGING_BUILD_PATH;
+    break;
+    case "development":
+      BUILD_PATH = env.DEV_BUILD_PATH;
+    break;
+  }
 }
 
 export default defineConfig({
