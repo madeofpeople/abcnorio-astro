0e20565f1c6553c0113ac26ef6e6efca8d2dab7b
Thiago de Mello Bueno <thiago@madeofpeople.org>
2026-04-28 11:21:14 +0000

    removes vestigious search path

--- src/lib/search/search.js
+++
@@ -1,9 +0,0 @@
-import { SEARCH_API } from '@util/paths.js';
-export async function getSearchResults( query='') {
-    const url = `${SEARCH_API}?q=${encodeURIComponent(query)?}&order=desc`;
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`Failed to fetch events: ${response.status}`);
-    }
-    return response.json();
-}
